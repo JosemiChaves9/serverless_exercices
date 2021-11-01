83da4d97d236d43d91e73f0fc0dd3e4451312a31
Josemi Chaves <josemichaves@gmail.com>
2021-11-01 16:40:51 +0100

    First exercise

---
+++ handler.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports.helloWorld = async (event) => {
+  let message = 'Hello world';
+  const name = event.queryStringParameters && event.queryStringParameters.name;
+
+  if (name !== null) {
+    message = `Hello ${name}`;
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: message,
+    }),
+  };
+};
